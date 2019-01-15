8f9ea6663eec728496ba84d3369ba19efd5d915b
Taylor Bell <taylor.bell@mail.mcgill.ca>
2019-01-15 17:28:16 -0500

    Rayleigh pol and Scat angle.
    Adding Rayleigh scattering polarization calculation code and reference frame rotation (2 methods) code to python files, and worked out the scattering angle computation in the jupyter notebook - will add this to the python files soon.

--- EPPE_Simulator/Polarimetry.py
+++ EPPE_Simulator/Polarimetry.py
@@ -3,33 +3,53 @@
 
 import numpy as np
 
-def lambertFlux(scatAngle):
-    """Compute the Lambertian scattering phase function.
+def lambert_scatter(theta, stokes):
+    """Compute the Lambertian scattering flux phase function.
     
     Args:
         scatAngle (ndarray): The scattering angle.
     
     Returns:
-        ndarray: The Lambertian phase function evaluated at scatAngle.
+        ndarray: The Lambertian scattering flux phase function evaluated at scatAngle.
     
     """
     
-    return np.abs((np.sin(scatAngle)+(np.pi-scatAngle)*np.cos(scatAngle))/np.pi)
+    return np.abs((np.sin(theta*np.pi/180)+(np.pi-theta*np.pi/180)*np.cos(theta*np.pi/180))/np.pi)*stokes
 
-
-def rayleighFlux(scatAngle):
-    """Compute the Rayleigh scattering phase function.
+def rayleigh_scatter(theta, stokes):
+    """Compute the Rayleigh scattering polarization phase function.
     
     Args:
         scatAngle (ndarray): The scattering angle.
     
     Returns:
-        ndarray: The Rayleigh phase function evaluated at scatAngle.
+        ndarray: The Rayleigh scattering polarization phase function evaluated at theta.
     
     """
-    
-    return np.sin(alpha)**2/(1+np.cos(alpha)**2)
+    matrix = 3./4.*np.array([[np.cos(theta*np.pi/180)**2+1, np.cos(theta*np.pi/180)**2-1, 0, 0],
+                            [np.cos(theta*np.pi/180)**2-1, np.cos(theta*np.pi/180)**2+1, 0, 0],
+                            [0, 0, 2*np.cos(theta*np.pi/180), 0],
+                            [0, 0, 0, 2*np.cos(theta*np.pi/180)]])
+    return np.matmul(matrix, stokes).reshape(4,-1)
 
+def rotate(phi, stokes):
+    Qprime = stokes[1]*np.cos(2*phi*np.pi/180) + stokes[2]*np.sin(2*phi*np.pi/180)
+    Uprime = -stokes[1]*np.sin(2*phi*np.pi/180) + stokes[2]*np.cos(2*phi*np.pi/180)
+    return np.array([stokes[0], Qprime, Uprime, stokes[3]]).reshape(4,-1)
 
-def rayleighPolarization(scatAngle):
-    return
+def get_rotate_matrix(phi):
+    return np.array([[1, 0, 0, 0],
+                     [0, np.cos(2*phi*np.pi/180), np.sin(2*phi*np.pi/180), 0],
+                     [0, -np.sin(2*phi*np.pi/180), np.cos(2*phi*np.pi/180), 0],
+                     [0, 0, 0, 0]])
+
+def rotate2(phi, stokes, rotate_matrix=None):
+    if rotate_matrix is None:
+        rotate_matrix = get_rotate_matrix(phi)
+    return np.matmul(rotate_matrix, stokes).reshape(4,-1)
+
+def xyz_to_scatAngle(r, dist):
+    d = np.array([dist, 0, 0])[:,np.newaxis]
+    scatAngle = 180/np.pi*np.arccos(np.sum(r*(d-r), axis=0)
+                                        /(np.sqrt(np.sum(r**2, axis=0))*np.sqrt(np.sum((d-r)**2, axis=0))))
+    return scatAngle
--- EPPE_Simulator/__init__.py
+++ EPPE_Simulator/__init__.py
@@ -1,6 +1,7 @@
 # Author: Taylor James Bell
-# Last Update: 2019-01-14
+# Last Update: 2019-01-15
 
 from .Systems import *
 from .Polarimetry import *
 from .EPPE import *
+from .KeplerOrbit import *
